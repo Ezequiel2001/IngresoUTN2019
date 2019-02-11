a812fef50d60a79e43e31d71c21fbe3d39a1d1b3
Ezequiel2001 <47321322+Ezequiel2001@users.noreply.github.com>
2019-02-11 12:35:13 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,9 @@
 function Mostrar()
 {
 //tomo la edad  
-
-
-
+var edad;
+edad=parseInt(document.getElementById("edad").value);
+if(edad<=17>13){
+    alert("La persona es adolescente");
+}
 }//FIN DE LA FUNCIÓN
