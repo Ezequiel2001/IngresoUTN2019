ac17d5a88576e2c3be2ea2a90307f5a1db46d1a6
Ezequiel2001 <47321322+Ezequiel2001@users.noreply.github.com>
2019-02-21 15:56:19 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,21 @@
-function Mostrar()
-{
-//tomo el mes
-var mesDelAño = document.getElementById('mes').value;
+function Mostrar() {
+    //tomo el mes
+    var mesDelAño = document.getElementById('mes').value;
+    switch (mesDelAño) {
+        case "Enero":
+            alert("Que comiences bien el año")
+            break;
+        case "Marzo":
+            alert("A clasess")
+            break;
+        case "Julio":
+            alert("Se vienen las vacaciones ")
+            break;
+        case "Diciembre":
+            alert("Felices Fiestas")
+            break;
 
-
+    }
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,15 @@
-function Mostrar()
-{
-//tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
+function Mostrar() {
+    //tomo el mes 
+    var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
-
+    //alert (mesDelAño);
+    switch (mesDelAño) {
+        case "Febrero":
+            alert("Este mes no tiene mas de 29 dias")
+            break;
+        default:
+            alert("Este mes tiene 30 o más días")
+            break;
+    }
 
 }//FIN DE LA FUNCIÓN
