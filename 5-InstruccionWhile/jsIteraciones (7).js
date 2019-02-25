48b6a6dab94a5f6317bdf4dfac3b9af501574d1b
Ezequiel2001 <47321322+Ezequiel2001@users.noreply.github.com>
2019-02-24 23:08:51 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,13 +1,23 @@
-function Mostrar()
-{
+function Mostrar() {
 
-	var contador=0;
-	var acumulador=0;
-	var respuesta='si';
-	whil
+	var contador = 0;
+	var acumulador = 0;
+	var numero;
+	var respuesta = 'si';
+	/*do {
+		respuesta = ("Quiere ingresar mas numeros?");
+		numero = parseInt(prompt("Ingrese un numero."));
+		acumulador = acumulador + numero;
 
+	} while (respuesta == "si");*/
+	respuesta = prompt("Quiere ingresar mas numeros?");
+	while (respuesta == "si") {
+		numero = parseInt(prompt("Ingrese un numero"));
+		acumulador = acumulador + numero;
+		contador;
+}
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
+	document.getElementById('suma').value = acumulador;
+	document.getElementById('promedio').value = acumulador / contador;
 
 }//FIN DE LA FUNCIÓN
